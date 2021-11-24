b2f14ceab5e2714c4abb1a9e140c386ffdc98c2f
kingtousick <wjdckd652@naver.com>
2021-11-24 10:31:17 +0900

    commitTest

--- fileDirMan.py
+++ fileDirMan.py
@@ -1,15 +1,12 @@
 import os
 
-def print_file_in_dir(root_dir, prefix):
+def print_file_in_dir(root_dir, prefix):  #파일경로 목록
     files = os.listdir(root_dir)
     for file in files:
         path = os.path.join(root_dir, file)
         print(prefix + path)
         if os.path.isdir(path):
             print_file_in_dir(path,"")
-
-
-
 if __name__=="__main__":
     root_dir ="C:/Users/LDCC/Documents/javaProject"
     print("--------------------------------")
